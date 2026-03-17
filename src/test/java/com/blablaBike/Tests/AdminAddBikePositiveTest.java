c39e23f11f1ea81bd6f8f5d2a525fa72aba2a747
Dariia Boiko <bdariia1320@gmail.com>
2026-03-17 21:59:49 +0100

    Add AdminAddBikePositiveTest and AdminDashboardPage and CatalogPage

---
+++ src/test/java/com/blablaBike/Tests/AdminAddBikePositiveTest.java
@@ -0,0 +1,47 @@
+package com.blablaBike.Tests;
+
+import com.blablaBike.core.TestBase;
+import com.blablaBike.pages.AdminDashboardPage;
+import com.blablaBike.pages.CatalogPage;
+import com.blablaBike.pages.window.AddBikePage;
+import org.checkerframework.checker.units.qual.C;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AdminAddBikePositiveTest extends TestBase {
+
+    AdminDashboardPage adminDashboardPage;
+    AddBikePage addBikePage;
+    CatalogPage catalogPage;
+
+    @BeforeEach
+    public void precondition() {
+        driver.get("https://blablabike-arx6.vercel.app/admin");
+        adminDashboardPage = new AdminDashboardPage(driver);
+        adminDashboardPage.verifyPageAdminDashboard();
+        addBikePage = new AddBikePage(driver);
+        catalogPage = new CatalogPage(driver);
+
+    }
+
+    @Test
+    public void addBikePositiveTest()
+    {
+//        new AdminDashboardPage(driver).clickOnAddBikeButton();
+//        new AddBikePage(driver).enterDataBike("TEST","Cross Classico 3","Bike for Urban ","104","https://surl.li/jtxuxr")
+//                .selectBikeCategory()
+//                .clickOnSaveButton();
+        new AddBikePage(driver) .clickOnCatalogButton();
+        new CatalogPage(driver).waitForCatalogPage();
+        assertTrue(catalogPage.isBikeAdded());
+
+
+
+
+    }
+
+
+
+}
